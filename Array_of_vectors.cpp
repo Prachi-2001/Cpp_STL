d774fa48202a076a7af1743458a80e20c6da94a5
Prachi Pachang <92040884+Prachi-2001@users.noreply.github.com>
2021-10-18 15:00:18 +0530

    Create Array_of_vectors.cpp

---
+++ Array_of_vectors.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Array of vectors 
+
+void printvec(vector<int> &v){
+		for(int i=0;i<v.size();i++){
+			cout<<v.at(i)<<" ";
+		}
+		cout<<"\n";
+}
+int main(){
+	int n;
+	cin>>n;
+    vector<int> v[n];  // create array of n vectors of any size 
+    for(int i=0;i<n;i++){
+    	int s;
+    	cin>>s;
+    	for(int j=0;j<s;j++){
+           int x;
+           cin>>x;
+           v[i].push_back(x);
+    	}
+    }
+    for(int i=0;i<n;i++){
+    	printvec(v[i]);
+    }
+	return 0;
+}
